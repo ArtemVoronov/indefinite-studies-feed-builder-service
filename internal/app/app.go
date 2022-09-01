00241624680eb115778e323e69dfcfaca63b3d2f
voronov <voronov54@gmail.com>
2022-09-01 17:47:41 +0700

    initial commit: added basic app structure

---
+++ internal/app/app.go
@@ -0,0 +1,55 @@
+package app
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/ArtemVoronov/indefinite-studies-feed-builder-service/internal/api/rest/v1/ping"
+	"github.com/ArtemVoronov/indefinite-studies-feed-builder-service/internal/services"
+	"github.com/ArtemVoronov/indefinite-studies-utils/pkg/app"
+	"github.com/ArtemVoronov/indefinite-studies-utils/pkg/services/auth"
+	"github.com/gin-contrib/expvar"
+	"github.com/gin-gonic/gin"
+)
+
+func Start() {
+	app.LoadEnv()
+	app.StartHTTP(setup, shutdown, app.HostHTTP(), createRestApi())
+}
+
+func setup() {
+}
+
+func shutdown() {
+}
+
+func createRestApi() *gin.Engine {
+	router := gin.Default()
+	gin.SetMode(app.Mode())
+	router.Use(app.Cors())
+	router.Use(gin.Logger())
+	router.Use(gin.CustomRecovery(func(c *gin.Context, recovered interface{}) {
+		if err, ok := recovered.(string); ok {
+			c.String(http.StatusInternalServerError, fmt.Sprintf("error: %s", err))
+		}
+		c.AbortWithStatus(http.StatusInternalServerError)
+	}))
+
+	// TODO: add permission controller by user role and user state
+	v1 := router.Group("/api/v1")
+
+	v1.GET("/feed/ping", ping.Ping)
+
+	authorized := router.Group("/api/v1")
+	authorized.Use(app.AuthReqired(authenicate))
+	{
+		authorized.GET("/feed/debug/vars", expvar.Handler())
+		authorized.GET("/feed/safe-ping", ping.SafePing)
+	}
+
+	return router
+}
+
+func authenicate(token string) (*auth.VerificationResult, error) {
+	return services.Instance().Auth().VerifyToken(token)
+}
---
+++ internal/services/services.go
@@ -0,0 +1,53 @@
+package services
+
+import (
+	"log"
+	"sync"
+
+	"github.com/ArtemVoronov/indefinite-studies-utils/pkg/app"
+	"github.com/ArtemVoronov/indefinite-studies-utils/pkg/services/auth"
+	"github.com/ArtemVoronov/indefinite-studies-utils/pkg/services/db"
+	"github.com/ArtemVoronov/indefinite-studies-utils/pkg/utils"
+)
+
+type Services struct {
+	auth *auth.AuthGRPCService
+	db   *db.PostgreSQLService
+}
+
+var once sync.Once
+var instance *Services
+
+func Instance() *Services {
+	once.Do(func() {
+		if instance == nil {
+			instance = createServices()
+		}
+	})
+	return instance
+}
+
+func createServices() *Services {
+	creds, err := app.LoadTLSCredentialsForClient(utils.EnvVar("AUTH_SERVICE_CLIENT_TLS_CERT_PATH"))
+	if err != nil {
+		log.Fatalf("unable to load TLS credentials")
+	}
+
+	return &Services{
+		auth: auth.CreateAuthGRPCService(utils.EnvVar("AUTH_SERVICE_GRPC_HOST")+":"+utils.EnvVar("AUTH_SERVICE_GRPC_PORT"), &creds),
+		db:   db.CreatePostgreSQLService(),
+	}
+}
+
+func (s *Services) Shutdown() {
+	s.auth.Shutdown()
+	s.db.Shutdown()
+}
+
+func (s *Services) DB() *db.PostgreSQLService {
+	return s.db
+}
+
+func (s *Services) Auth() *auth.AuthGRPCService {
+	return s.auth
+}
