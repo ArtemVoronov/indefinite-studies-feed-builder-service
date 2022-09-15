6f8ff874e21a1eb4b7f0c210d4986288127ba981
voronov <voronov54@gmail.com>
2022-09-15 11:49:51 +0700

    added feed sync an app starting

--- internal/app/app.go
+++ internal/app/app.go
@@ -27,6 +27,9 @@ func Start() {
 
 func setup() {
 	services.Instance()
+	if err := services.Instance().Feed().Sync(); err != nil {
+		log.Fatalf("unable to sync feed: %v", err)
+	}
 }
 
 func shutdown() {
