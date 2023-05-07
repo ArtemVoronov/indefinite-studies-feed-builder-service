e71c532a6aef60edab3f88c433b2d71c7da400da
voronov <voronov54@gmail.com>
2023-05-07 11:35:23 +0700

    code cleaning

---
+++ run.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+down() {
+    docker-compose down
+}
+
+purge() {
+    docker volume rm indefinite-studies-feed-builder-service_redis-volume
+}  
+
+build() {
+    docker-compose build api
+}
+
+start() {
+    docker-compose up -d
+}
+
+tail() {
+    docker-compose logs -f
+}
+
+case "$1" in
+  start)
+    down
+    purge
+    build
+    start
+    tail
+    ;;
+  stop)
+    down
+    ;;
+  tail)
+    tail
+    ;;
+  purge)
+    down
+    purge
+    ;;
+  *)
+    echo "Usage: $0 {start|stop|purge|tail}"
+esac
